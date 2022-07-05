a97146fe186bd7b3343c53ec6fdf0fc3072d7bc5
GaleanoJorge <juandavidrianoperez@gmail.com>
2022-07-05 18:47:01 -0500

    encriptación de contrasña

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const db = require('../config/config');
+const crypto = require('crypto');
 
 const User = {};
 
@@ -13,6 +14,10 @@ User.getAll = () => {
 }
 
 User.create = (user) => {
+
+    const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
+    user.password = myPasswordHashed;
+
     const sql = `
     INSERT INTO
         users(
