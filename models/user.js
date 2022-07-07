e0f731c1f6bf3836131529308a65c10e90f9ef8f
GaleanoJorge <juandavidrianoperez@gmail.com>
2022-07-06 19:43:07 -0500

    login y tokenizado

--- models/user.js
+++ models/user.js
@@ -13,6 +13,45 @@ User.getAll = () => {
     return db.manyOrNone(sql);
 }
 
+User.findByEmail = (email) => {
+    const sql = `
+    SELECT
+        id,
+        email,
+        name,
+        lastname,
+        image,
+        phone,
+        password,
+        session_token
+    FROM
+        users
+    WHERE
+        email = $1
+    `
+
+    return db.oneOrNone(sql, email);
+}
+
+User.findById = (id, callback) => {
+    const sql = `
+    SELECT
+        id,
+        email,
+        name,
+        lastname,
+        image,
+        phone,
+        password,
+        session_token
+    FROM
+        users
+    WHERE
+        id = $1
+    `;
+    return db.oneOrNone(sql, id).then(user => { callback(null, user) });
+}
+
 User.create = (user) => {
 
     const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
@@ -45,6 +84,14 @@ User.create = (user) => {
         new Date(),
         new Date()
     ]);
+}
+
+User.isPasswordMached = (userPassword, hash) => {
+    const myPasswordHashed = crypto.createHash('md5').update(userPassword).digest('hex');
+    if(myPasswordHashed === hash) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = User;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,4 +6,5 @@ module.exports = (app) => {
 
     // GUARDAR DATOS
     app.post('/api/users/create', UsersController.register);
+    app.post('/api/users/login', UsersController.login);
 }
