517f44985315a4f0ae43d97da6788cbd913abd4d
GaleanoJorge <juandavidrianoperez@gmail.com>
2022-07-09 19:05:01 -0500

    se agrega en cnsulta sql los roles asociado como un json anexo

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -68,7 +68,10 @@ module.exports = {
                     phone: myUser.phone,
                     image: myUser.image,
                     session_token: `JWT ${token}`,
+                    roles: myUser.roles,
                 }
+
+                console.log(`USUARIO ENVIADO ${data}`)
 
                 return res.status(201).json({
                     success: true,
